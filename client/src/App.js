14a201ef1d1a1ebdd834c6a2d41ca2749fb94a12
Olive <olivelliott48@gmail.com>
2022-09-30 23:15:02 -0400

    Initialize and test homepage render

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,34 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import {
   ApolloProvider,
   ApolloClient,
   InMemoryCache,
   createHttpLink,
-} from '@apollo/client'
+} from "@apollo/client";
 // import { setContext } from '@apollo/client/link/context'
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-
-
+import Home from "./pages/Home";
 
 function App() {
   const httpLink = createHttpLink({
-    uri: '/graphql',
-  })
+    uri: "/graphql",
+  });
 
   const client = new ApolloClient({
     // link: authLink.concat(httpLink),
     cache: new InMemoryCache(),
-  })
-
+  });
 
   return (
     <ApolloProvider client={client}>
       <Router>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+        </Routes>
       </Router>
     </ApolloProvider>
-  )
+  );
 }
 
 export default App;
