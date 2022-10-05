67e80bac5ab1d4e9d922ffabdad7bd9f577b6742
Olive <olivelliott48@gmail.com>
2022-10-04 22:22:29 -0400

    Create seeds | Add article model to typeDefs & Resolvers

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1,26 +1,37 @@
-const db = require('./connection');
-const { User, Article } = require('../models');
+const db = require("./connection");
+const { User, Article } = require("../models");
 
-db.once('open', async () => {
-    await User.deleteMany();
+db.once("open", async () => {
+  await User.deleteMany();
+  const users = await User.insertMany([
+    {
+      firstName: "Pamela",
+      lastName: "Washington",
+      email: "pamela@testmail.com",
+      password: "password12345",
+    },
 
-    await User.create({
-        firstName: 'Pamela',
-        lastName: 'Washington',
-        email: 'pamela@testmail.com',
-        password: 'password12345',
-      });
-    
-      await User.create({
-        firstName: 'Elijah',
-        lastName: 'Holt',
-        email: 'eholt@testmail.com',
-        password: 'password12345'
-      });
-    
-      console.log('users seeded ✔️');
-    
-      process.exit();
-    
+    {
+      firstName: "Elijah",
+      lastName: "Holt",
+      email: "eholt@testmail.com",
+      password: "password12345",
+    },
+  ]);
+  console.log("users seeded ✔️");
 
-} )
+  await Article.deleteMany();
+
+  const articles = await Article.insertMany([
+    {
+      title: "IOS16 Updates & Features",
+      overview: "A breakdown of the latest changes in Apple IOS16 updates",
+      image: "/images/ios_16.png",
+      user: users[0],
+    },
+  ]);
+  console.log("articles seeded ✔️");
+
+
+  process.exit();
+});
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,27 +1,29 @@
 const { gql } = require("apollo-server-express");
 
-const typeDefs = gql `
-    type User {
-        _id: ID
-        firstName: String
-        lastName: String
-        email: String
-    }
+const typeDefs = gql`
 
     type Article {
-        _id: ID
-        title: String 
-        overview: String
-        image: String
-        datePosted: String
-        user: [User]
-    
+    _id: ID
+    title: String
+    overview: String
+    image: String
+    datePosted: String
+    user: [User]
     }
 
-    type Query {
-        users: User
-        articles: Article
-    }
+  type User {
+    _id: ID
+    firstName: String
+    lastName: String
+    email: String
+  }
+
+
+  type Query {
+    users: User
+    articles: Article
+  }
+
 `;
 
 module.exports = typeDefs;
